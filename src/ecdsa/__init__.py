963fd693c7dbbaef418f1b5ba284706c82bb717c
André Lochotzke <andre.lochotzke@cgi.com>
2019-01-30 10:42:36 +0100

    Also add new curves to __init__

--- src/ecdsa/__init__.py
+++ src/ecdsa/__init__.py
@@ -1,5 +1,6 @@
 from .keys import SigningKey, VerifyingKey, BadSignatureError, BadDigestError
-from .curves import NIST192p, NIST224p, NIST256p, NIST384p, NIST521p, SECP256k1
+from .curves import (NIST192p, NIST224p, NIST256p, NIST384p, NIST521p, SECP256k1,
+                     BRAINPOOLP256r1, BRAINPOOLP384r1, BRAINPOOLP512r1)
 
 # This code comes from http://github.com/warner/python-ecdsa
 from ._version import get_versions
@@ -10,5 +11,6 @@ __all__ = ["curves", "der", "ecdsa", "ellipticcurve", "keys", "numbertheory",
            "test_pyecdsa", "util", "six"]
 
 _hush_pyflakes = [SigningKey, VerifyingKey, BadSignatureError, BadDigestError,
-                  NIST192p, NIST224p, NIST256p, NIST384p, NIST521p, SECP256k1]
+                  NIST192p, NIST224p, NIST256p, NIST384p, NIST521p, SECP256k1,
+                  BRAINPOOLP256r1, BRAINPOOLP384r1, BRAINPOOLP512r1]
 del _hush_pyflakes
